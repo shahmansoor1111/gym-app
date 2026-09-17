ccf69cc2ca53a1b6b764cb15832c38c018e29b24
shahmansoor1111 <shahmansoor0316@gmail.com>
2026-09-17 16:18:51 +0500

    Fix gym project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import heroAthlete   from '../Images/gym2.jpg';
-import gymTraining   from '../images/gymTraining.avif';
+import gymTraining   from '../images/gym.avif';
 import exChest1      from '../images/exChest1.avif';
 import exBack1       from '../images/exback1.avif';
 import exLegs1       from '../images/legs.avif';
